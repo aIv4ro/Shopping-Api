be2d5c39601c55b24a5752be8098e69b633e16d2
Alvaro <alvaromagu2002@gmail.com>
2022-09-02 13:45:06 +0200

    - pruebas

--- src/routes/index.route.js
+++ src/routes/index.route.js
@@ -8,9 +8,9 @@ import { auth } from '../middlewares/auth.js';
 const router = Express.Router();
 
 router.use('/', (req, res) => res.send('Welcom to Shopping'));
-router.use('/api/auth', authRouter);
+/*router.use('/api/auth', authRouter);
 router.use('/api/user', auth, userRouter);
 router.use('/api/product', auth, productRouter);
-router.use('/api/order', auth, orderRouter);
+router.use('/api/order', auth, orderRouter);*/
 
 export default router;
